5c960083b1c38ee0586251c4516bbc9a3cf4c55b
Darian <darian.mata@gmail.com>
2024-04-15 14:48:31 -0600

    Assignment 2 - Test Commit

---
+++ Main_Program.py
@@ -0,0 +1,6 @@
+# OSU Email: matafe@oregonstate.edu
+# Course: CS361 - Software Engineering I
+# Assignment: Course Project
+# Description:
+#   Assignment 2 - Environment Set-up. Created GitHub repository and made a test-commit with file details.
+
